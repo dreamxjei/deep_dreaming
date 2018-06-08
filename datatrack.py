e08ca8cf1c6e13dc6526ad8f4c2e1f748656dd0e
Jinchi Wei <jwei@lambda>
2018-06-08 15:41:24 -0400

    data and model tracks up to date

--- datatrack.py
+++ datatrack.py
@@ -7,16 +7,17 @@ import numpy as np
 from torch.utils.data.dataset import Dataset
 from skimage import io, color
 
+
 # data directory
 
 dataset_dir = 'dataset/100_20_30'
 directories = {
-    'no_train' : 'no_THA_train',
-    'yes_train' : 'yes_THA_train',
-    'no_val' : 'no_THA_val',
-    'yes_val' : 'yes_THA_val',
-    'no_test' : 'no_THA_test',
-    'yes_test' : 'yes_THA_test'
+    'train_0' : 'no_THA_train',
+    'train_1' : 'yes_THA_train',
+    'val_0' : 'no_THA_val',
+    'val_1' : 'yes_THA_val',
+    'test_0' : 'no_THA_test',
+    'test_1' : 'yes_THA_test'
 }
 
 result_classes = {
@@ -24,8 +25,53 @@ result_classes = {
     1 : 'yes_THA'
 }
 
-def main():
-    # lorem ipsum
+
+class importData():
+    def __init__(self, mode, transform=None):
+        """
+        Args:
+            transform (callable, optional): Optional transform to be applied on a sample.
+        """
+        self.transform = transform
+        self.sample_paths = []
+        self._init(mode)
+
+    def __len__(self):
+        return len(self.sample_paths)
+
+    def __getitem_(self, idx):
+        img_path, label = self.sample_paths[idx]
+        x = io.imread(img_path)
+
+        # in order to make everything in RGB (x, y, 3) dimension
+        shape = x.shape
+        if len(shape) == 3 and shape[2] > 3:  # for cases (x, y, 4)
+            x = x[:, :, :3]
+        elif len(shape) == 2:  # for cases (x, y)
+            x = color.gray2rgb(x)
+
+        # in order to make sure images are read as uint8
+        if x.dtype != np.uint8:
+            x = x.astype(np.uint8)
+
+        if self.transform:
+            x = self.transform(x)
+
+        return(x, label)
+
+    def _init(self, mode):
+        dir_0 = os.path.join(dataset_dir, directories[mode + '_0'])
+        dir_1 = os.path.join(dataset_dir, directories[mode + '_1'])
+
+        # Result class iteration
+        for class_num in result_classes:
+            samples = os.listdir('dir_' + str(class_num))
+            for sample in samples:
+                if not sample.startswith('.'):  # avoid .DS_Store
+                    img_path = os.path.join('dir_' + str(class_num), sample)
+                    self.sample_paths.append((img_path, class_num))
+
+
 
 if __name__ == '__main__':
-    main()
+    print('Main called')
--- modeltrack.py
+++ modeltrack.py
@@ -1,1 +1,103 @@
 # model track by mxj
+
+# a python file to hold each individual model's code
+
+import torch.nn as nn
+from torchvision import models
+
+
+def ResNet18_pretrained(n_classes, freeze=True):
+    model = models.__dict__['resnet18'](pretrained=True)
+    # if weights are not frozen, train
+    for param in model.parameters():
+        if freeze:
+            param.requires_grad = False
+        else:
+            param.requires_grad = True
+
+    # change last layer to output n_classes
+    num_filters = model.fc.in_features
+    model.fc = nn.Linear(num_filters, n_classes)
+    return model
+
+
+def inception_v3_pretrained(n_classes, freeze=True):
+    model = models.__dict__['inception_v3'](pretrained=True)
+    # if weights are not frozen, train
+    for param in model.parameters():
+        if freeze:
+            param.requires_grad = False
+        else:
+            param.requires_grad = True
+
+    # change last layer to output n_classes
+    num_filters = model.fc.in_features
+    model.fc = nn.Linear(num_filters, n_classes)
+    return model
+
+
+def AlexNet_pretrained(n_classes, freeze=True):
+    model = models.__dict__['alexnet'](pretrained=True)
+    # if weights are not frozen, train
+    for param in model.parameters():
+        if freeze:
+            param.requires_grad = False
+        else:
+            param.requires_grad = True
+
+    # change last layer to output n_classes
+    lastclasslayer = str(len(model.classifier.modules) - 1)
+    num_filters = model.classifier.modules[lastclasslayer].in_features
+    model.classifier.modules[lastclasslayer] = nn.Linear(num_filters, n_classes)
+    return model
+
+
+def SqueezeNet_pretrained(n_classes, freeze=True):
+    model = models.__dict__['squeezenet1_1'](pretrained=True)
+    # if weights are not frozen, train
+    for param in model.parameters():
+        if freeze:
+            param.requires_grad = False
+        else:
+            param.requires_grad = True
+
+    # change last layer to output n_classes
+    model.classifier.modules['1'] = nn.Conv2d(512, n_classes, kernel_size=(1, 1), stride=(1, 1))
+    model.num_classes = n_classes
+    return model
+
+
+def VGGNet_pretrained(n_classes, freeze=True):
+    model = models.__dict__['vgg16'](pretrained=True)
+    # if weights are not frozen, train
+    for param in model.parameters():
+        if freeze:
+            param.requires_grad = False
+        else:
+            param.requires_grad = True
+
+    # change last layer to output n_classes
+    lastclasslayer = str(len(model.classifier.modules) - 1)
+    num_filters = model.classifier.modules[lastclasslayer].in_features
+    model.classifier.modules[lastclasslayer] = nn.Linear(num_filters, n_classes)
+    return model
+
+
+def DenseNet_pretrained(n_classes, freeze=True):
+    model = models.__dict__['densenet161'](pretrained=True)
+    # if weights are not frozen, train
+    for param in model.parameters():
+        if freeze:
+            param.requires_grad = False
+        else:
+            param.requires_grad = True
+
+    # change last layer to output n_classes
+    num_filters = model.classifier.in_features
+    model.classifier = nn.Linear(num_filters, n_classes)
+    return model
+
+
+if __name__ == '__main__':
+    model = ResNet18_pretrained(2)
+    print('Main called')
