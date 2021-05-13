ac2ac687e13d851c0cc3df9d0c880aff1646bd2e
Jinchi Wei <mrjinch@gmail.com>
2021-05-13 10:10:22 -0400

    updated merge sort

--- tutorial_project/merge_sort.py
+++ tutorial_project/merge_sort.py
@@ -1,10 +1,36 @@
-def merge_sort(list):
-    print('lorem ipsum')
-    list = [10, 8, 2, 3, 5, 1, 4, 7, 6, 9]
-    len(list)/2
+def merge_sort(x):
+    if len(x) > 1:
+        mid = len(x)//2
+        a = x[:mid]
+        b = x[mid:]
+
+        # split until length = 1, where function will stop being called
+        a = merge_sort(a)
+        b = merge_sort(b)
+
+        # reinitialize x, now that all elements are all in a or b
+        x = []
+
+        # compare values and delete. iterate
+        while len(a) > 0 and len(b) > 0:
+            if a[0] <= b[0]:
+                x.append(a[0])
+                del a[0]
+            else:
+                x.append(b[0])
+                del b[0]
+
+        # add any final stragglers. max 2
+        for e in a:
+            x.append(e)
+        for e in b:
+            x.append(e)
+
+    return x
 
 
-# x = [8, 3, 6, 9, 1, 10, 4]
-user_input = input('Specify a list of numbers separated by spaces to sort: ')
+user_input = input('Specify a list of numbers to sort separated by spaces: ')
 x = [int(n) for n in user_input.split()]
-merge_sort(x)
+x = merge_sort(x)
+
+print(x)
